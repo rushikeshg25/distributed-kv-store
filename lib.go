7583e85aa0838ddf0ff8ae1994cea40a09e9e1fd
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-12-27 22:05:51 +0530

    feat: upsert with unix epoch

--- lib.go
+++ lib.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"log"
 	"time"
 )
 
-//Cruud
-
-var 
 
 func KeyValueCreationOrUpdate(key string, value any){
-	// expiresAt:=time.Now()
-	query,err:=Db.Prepare(`
-	INSERT INTO kv (kv_key,kv_value) VALUES (?,?)
-	`)
+	expiresAt:=time.Now().Add(time.Minute*10).Unix()
+	query,err:= Db.Prepare(`INSERT INTO kv (kv_key, kv_value, kv_expires_at) 
+	VALUES (?, ?, ?) 
+	ON DUPLICATE KEY UPDATE 
+	kv_value = VALUES(kv_value),
+	kv_expires_at = VALUES(kv_expires_at)`)
+	if err!=nil{
+		log.Fatalf("%s",err)
+	}
+	_, err = query.Exec("key1", []byte("value"), expiresAt)
+	if(err!=nil){
+		log.Fatalf("%s",err)
+	}
 }
