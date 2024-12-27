8be6b979c5c1f61266c97a2f11124f29095d0192
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-12-27 22:32:42 +0530

    feat: Get

--- lib.go
+++ lib.go
@@ -21,3 +21,23 @@ func KeyValueCreationOrUpdate(key string, value any){
 		log.Fatalf("%s",err)
 	}
 }
+
+func GetKeyValue(key string) any{
+	var value any
+	var expiresAt int64
+	err:=Db.QueryRow(`SELECT (kv_value,kv_expires_at) FROM kv WHERE kv_key=key`).Scan(&value,&expiresAt)
+	if(err!=nil){
+		log.Fatalf("%s",err)
+	}
+	if(time.Now().Unix()>expiresAt){
+		_,err:=Db.Exec(`DELETE FROM kv WHERE kv_key=key`,key)
+		if(err!=nil){
+			log.Fatalf("%s",err)
+		}
+		return nil
+	}
+	return value
+}
+
+//Incase of Inme DB use the <20% rule to delete keys on cron job 
+func DeleteKeyValue(key string){}
